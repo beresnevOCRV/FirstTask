fcfc68c8b098c4549455333465555e9659102a4c
ivan.beresnev <ivan.beresnev@OCRV.RU>
2022-08-29 18:31:19 +0300

    FirstTask commit3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,12 @@ public class App
     public static void main( String[] args ) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
         AutoFactory autoFactory = context.getBean("auto_factory", AutoFactory.class);
+        AutoFactory autoFactory1 = context.getBean("auto_factory1", AutoFactory.class);
+
         autoFactory.runProduction();
+        System.out.println();
+        autoFactory1.runProduction();
     }
 }
